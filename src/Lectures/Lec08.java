b7b00efd12b65351b08ed15a0d5c847822c3a792
kalyousefi <kalyousefi@hotmail.com>
2021-04-04 21:30:39 +0300

    Lecture 8 added

--- src/Lectures/Lec08.java
+++ src/Lectures/Lec08.java
@@ -1,22 +1,59 @@
 /* 
-  Lecture 8: Methods
+  Lecture 8: 2D Arrays
   
-  source 1: https://harmash.com/java/java-methods/
-  source 2: https://www.w3schools.com/java/java_methods.asp
-
+  source 1: https://harmash.com/java/java-arrays/
+  source 2: https://www.w3schools.com/java/java_arrays.asp
+  
 */
 package Lectures;
 
 public class Lec08 {
 
 	public static void main(String[] args) {
-		
+
+		// 1D Array
+		String a[] ={"Khaled", "Ali", "Mohammed"};
 		// 2D Array
 		
-		// Creating a method
+		// creating 2d array
+		String arr2d[][] = { {"Khaled", "Ali", "Mohammed"},
+							 {"Riyadh", "Jeddah", "Arar", "Altaif"}};
 		
+		// creating 2d array with known size
+		int arr3[][] = new int[4][7];
 		
+		System.out.println(arr2d[0][0]); // Khaled
+		System.out.println(arr2d[1][0]); // Riyadh
+		
+		System.out.println(arr2d.length); // 
+		System.out.println(arr2d[1].length); // 
+		
+		for(int i = 0; i < arr2d.length; i++)
+			for(int j = 0; j < arr2d[i].length; j++)
+				System.out.println(arr2d[i][j]);
 
+		for(int i = 0; i < arr2d.length; i++) {
+			for(int j = 0; j < arr2d[i].length; j++)
+				System.out.print(arr2d[i][j] + "\t");
+			System.out.println();
+		}
+		
+		for(int i = 0; i < arr3.length; i++)
+			for(int j = 0; j < arr3[i].length; j++)
+				System.out.println(arr3[i][j]);
+		
+		int counter = 1;
+		for(int i = 0; i < arr3.length; i++)
+			for(int j = 0; j < arr3[i].length; j++) {
+				arr3[i][j] = counter;
+				counter++;
+			}
+		
+		for(int i = 0; i < arr3.length; i++) {
+			for(int j = 0; j < arr3[i].length; j++)
+			System.out.print(arr3[i][j] + "\t");
+		System.out.println();
+		}
 	}
 
 }
---
+++ src/Lectures/Lec09.java
@@ -0,0 +1,20 @@
+/* 
+  Lecture 8: Methods
+  
+  source 1: https://harmash.com/java/java-methods/
+  source 2: https://www.w3schools.com/java/java_methods.asp
+
+*/
+package Lectures;
+
+public class Lec09 {
+
+	public static void main(String[] args) {
+
+		// Creating a method
+		
+		
+
+	}
+
+}
