ebaa82baf6c0e17060d61698aad205f1f1298c96
kalyousefi <kalyousefi@hotmail.com>
2021-09-09 12:02:16 +0300

    adding lecture 09

--- src/Lectures/Lec09.java
+++ src/Lectures/Lec09.java
@@ -8,13 +8,50 @@
 package Lectures;
 
 public class Lec09 {
-
-	public static void main(String[] args) {
-
-		// Creating a method
-		
-		
-
+	
+	// Simple method 1
+	void greeting()
+	{
+		System.out.println("Hello from greeting method");
 	}
 
+	// Simple static method 2
+	static void  greeting2()
+	{
+		System.out.println("Hello from the static greeting2 method");
+	}
+
+	// Method 3: take 2 input numbers and output the sum
+	static int  add(int a, int b)
+	{
+		return (a+b);
+	}
+	
+	// Method 4: take 1 input number and output the factorial
+	static int factorial(int n)
+	{
+		int factorial = 1;
+		for(int i=2; i<=n; i++)
+			factorial = factorial * i;
+		
+		return factorial;
+	}
+
+	public static void main(String[] args) {
+		
+		// create an object
+		Lec09 obj = new Lec09();
+		
+		// calling the object method
+		obj.greeting();
+		
+		// calling the static method
+		greeting2();
+		
+		// using the add method
+		System.out.println(add(3,6));
+		
+		// using the factorial method
+		System.out.println(factorial(5));		
+	}
 }
